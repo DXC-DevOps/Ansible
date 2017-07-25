e6d71dd25896e4730c1fc870b39b9b464c38ca11
bhagyagunasekar <bgunasekar2@csc.com>
2017-07-25 12:58:32 +0530

    Update Windows2012R2.ps1

--- Windows2012R2.ps1
+++ Windows2012R2.ps1
@@ -10,3 +10,17 @@ Add-Type -assembly "system.io.compression.filesystem"
 [io.compression.zipfile]::ExtractToDirectory($BackUpPath, $destination)
 cd c:\Temp
 $ssminstall=.\Ec2Install.exe /quiet /norestart
+write-host "Enabling CloudWatch logs"
+$EC2SettingsFile="C:\Program Files\Amazon\Ec2ConfigService\Settings\Config.xml"
+$xml = [xml](get-content $EC2SettingsFile)
+$xmlElement = $xml.get_DocumentElement()
+$xmlElementToModify = $xmlElement.Plugins
+
+foreach ($element in $xmlElementToModify.Plugin)
+{
+    if ($element.name -eq "AWS.EC2.Windows.CloudWatch.PlugIn")
+    {
+        $element.State="Enabled"
+    }
+}
+$xml.Save($EC2SettingsFile)

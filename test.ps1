e76599cec1854c39c56a18fe4b011d97d1c7bb4f
bhagyagunasekar <bgunasekar2@csc.com>
2017-07-31 18:25:28 +0530

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -1,4 +1,4 @@
-New-Item -Path c:\testing -ItemType directory
+New-Item -Path c:\Temp -ItemType directory
 write-host "Installing and Enabling SSM agent"
 Invoke-WebRequest -Uri "https://s3.amazonaws.com/ec2-downloads-windows/EC2Config/EC2Install.zip" -OutFile "C:\Temp\EC2Install.zip"
 $BackUpPath = "C:\Temp\EC2Install.zip"

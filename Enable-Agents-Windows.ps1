fc139807be50920d57cc9fa1ffc768645301d818
bhagyagunasekar <bgunasekar2@csc.com>
2017-08-02 15:27:19 +0530

    Update and rename test.ps1 to Enable-Agents-Windows.ps1

--- Enable-Agents-Windows.ps1
+++ Enable-Agents-Windows.ps1
@@ -1,4 +1,7 @@
 New-Item -Path c:\awstemp -ItemType directory
+Invoke-WebRequest -Uri "https://github.com/bhagyagunasekar/Ansible/raw/master/WindowsSensor_5121_NGAV_INCLUDED.exe" -OutFile "C:\awstemp\WindowsSensor_5121_NGAV_INCLUDED.exe"
+cd c:\awstemp
+$installer=.\WindowsSensor_5121_NGAV_INCLUDED.exe /install /quiet /norestart CID="35C43E7262224DFB9AA9F142596987E5-E7"
 write-host "Installing and Enabling SSM agent"
 Invoke-WebRequest -Uri "https://s3.amazonaws.com/ec2-downloads-windows/EC2Config/EC2Install.zip" -OutFile "C:\awstemp\EC2Install.zip"
 $BackUpPath = "C:\awstemp\EC2Install.zip"

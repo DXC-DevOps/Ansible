9d4c50cd4fb8c6c059f63d6b471b1c2048ef95b8
bhagyagunasekar <bgunasekar2@csc.com>
2017-08-02 11:14:23 +0530

    Update aws-cli.ps1

--- aws-cli.ps1
+++ aws-cli.ps1
@@ -1,1 +1,1 @@
-Invoke-WebRequest -Uri "https://github.com/bhagyagunasekar/Ansible/raw/master/AWSCLI64.msi" -OutFile "C:\awstemp\AWSCLI64.msi"
+Invoke-WebRequest -Uri "https://github.com/DXC-DevOps/Ansible/raw/CSCSOE/AWSCLI64.msi" -OutFile "C:\awstemp\AWSCLI64.msi"

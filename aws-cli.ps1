dddf223e620ef3430d28dad44801f49e7fbc736e
bhagyagunasekar <bgunasekar2@csc.com>
2017-08-02 12:11:51 +0530

    Update aws-cli.ps1

--- aws-cli.ps1
+++ aws-cli.ps1
@@ -1,3 +1,3 @@
 Invoke-WebRequest -Uri "https://github.com/DXC-DevOps/Ansible/raw/CSCSOE/AWSCLI64.msi" -OutFile "C:\awstemp\AWSCLI64.msi"
-Invoke-WebRequest -Uri "https://www.python.org/ftp/python/3.5.4/python-3.5.4rc1.exe" -OutFile "C:\awstemp\python-3.5.4rc1.exe"
+
 

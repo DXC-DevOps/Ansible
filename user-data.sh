b891cdc34154bc545da884c3fb15f1d2560194b8
bhagyagunasekar <bgunasekar2@csc.com>
2017-08-03 15:18:11 +0530

    Update user-data.sh

--- user-data.sh
+++ user-data.sh
@@ -3,4 +3,9 @@ curl https://s3.amazonaws.com//aws-cloudwatch/downloads/latest/awslogs-agent-set
 curl https://s3.amazonaws.com//aws-cloudwatch/downloads/latest/AgentDependencies.tar.gz -O
 tar xvf AgentDependencies.tar.gz -C /tmp/
 sudo python ./awslogs-agent-setup.py -n -r us-west-2 -c s3://test-log-rarchivelogsbucket-vff02p5x5izl/my-config-file
-
+sudo yum install wget -y
+sudo wget https://bootstrap.pypa.io/get-pip.py
+python get-pip.py --user
+pip install awscli --upgrade --user
+EC2_INSTANCE_ID=`wget -q -O - http://169.254.169.254/latest/meta-data/instance-id`
+aws ec2 monitor-instances --instance-ids $EC2_INSTANCE_ID --region us-west-2

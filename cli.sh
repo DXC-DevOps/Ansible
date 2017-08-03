ffca8f98c589fbc50f2ef95f8ed0d009f75ef4d8
bhagyagunasekar <bgunasekar2@csc.com>
2017-08-03 16:21:01 +0530

    Update cli.sh

--- cli.sh
+++ cli.sh
@@ -2,4 +2,4 @@
 sudo yum install wget -y
 sudo wget https://bootstrap.pypa.io/get-pip.py
 python get-pip.py --user
-EC2_INSTANCE_ID=`wget -q -O - http://169.254.169.254/latest/meta-data/instance-id`
+

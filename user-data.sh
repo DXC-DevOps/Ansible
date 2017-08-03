62e98ecd2f384a63a578adda3144bad11d8e10b7
bhagyagunasekar <bgunasekar2@csc.com>
2017-08-03 16:25:23 +0530

    Update user-data.sh

--- user-data.sh
+++ user-data.sh
@@ -3,3 +3,6 @@ curl https://s3.amazonaws.com//aws-cloudwatch/downloads/latest/awslogs-agent-set
 curl https://s3.amazonaws.com//aws-cloudwatch/downloads/latest/AgentDependencies.tar.gz -O
 tar xvf AgentDependencies.tar.gz -C /tmp/
 sudo python ./awslogs-agent-setup.py -n -r us-west-2 -c s3://test-log-rarchivelogsbucket-vff02p5x5izl/my-config-file
+sudo yum install wget -y
+sudo wget https://bootstrap.pypa.io/get-pip.py
+python get-pip.py --user

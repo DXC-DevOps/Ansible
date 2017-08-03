677c88e04901bc51009db0b54858d7388368ed16
bhagyagunasekar <bgunasekar2@csc.com>
2017-08-03 12:41:11 +0530

    Update user-data.sh

--- user-data.sh
+++ user-data.sh
@@ -4,6 +4,7 @@ curl https://s3.amazonaws.com//aws-cloudwatch/downloads/latest/AgentDependencies
 tar xvf AgentDependencies.tar.gz -C /tmp/
 sudo python ./awslogs-agent-setup.py -n -r us-west-2 -c s3://test-log-rarchivelogsbucket-vff02p5x5izl/my-config-file
 echo "Enabling monitoring for instance"
-EC2_INSTANCE_ID="`wget -q -O - http://169.254.169.254/latest/meta-data/instance-id`"
+sudo yum install wget -y
 pip install awscli --upgrade --user
+EC2_INSTANCE_ID=wget -q -O - http://169.254.169.254/latest/meta-data/instance-id
 aws ec2 monitor-instances --instance-ids $EC2_INSTANCE_ID --region us-west-2

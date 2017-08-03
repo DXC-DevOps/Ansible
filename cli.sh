0003369a2a5f61d34e1fc549f01705682473901f
bhagyagunasekar <bgunasekar2@csc.com>
2017-08-03 15:29:12 +0530

    Update cli.sh

--- cli.sh
+++ cli.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 sudo yum install wget -y
+sudo wget https://bootstrap.pypa.io/get-pip.py
+python get-pip.py --user
 pip install awscli --upgrade --user
 EC2_INSTANCE_ID=`wget -q -O - http://169.254.169.254/latest/meta-data/instance-id`
 aws ec2 monitor-instances --instance-ids $EC2_INSTANCE_ID --region us-west-2

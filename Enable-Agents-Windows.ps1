fe384fd12ae1ff86fb545fa0e13f63b82c699a76
bhagyagunasekar <bgunasekar2@csc.com>
2017-08-04 16:43:14 +0530

    Update Enable-Agents-Windows.ps1

--- Enable-Agents-Windows.ps1
+++ Enable-Agents-Windows.ps1
@@ -28,4 +28,5 @@ foreach ($element in $xmlElementToModify.Plugin)
     }
 }
 $xml.Save($EC2SettingsFile)
-restart-service AmazonSSMAgent
+if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
+    restart-service AmazonSSMAgent

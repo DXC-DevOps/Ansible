b93589c7578010db04b4be9e98b4ca92fd788255
bhagyagunasekar <bgunasekar2@csc.com>
2017-08-04 14:15:43 +0530

    Update aws-cli.ps1

--- aws-cli.ps1
+++ aws-cli.ps1
@@ -1,4 +1,2 @@
 Invoke-WebRequest -Uri "https://github.com/DXC-DevOps/Ansible/raw/CSCSOE/AWSCLI64.msi" -OutFile "C:\awstemp\AWSCLI64.msi"
-write-host "Enabling Monitoring"
-EC2_INSTANCE_ID=`wget -q -O - http://169.254.169.254/latest/meta-data/instance-id`
-aws ec2 monitor-instances --instance-ids $EC2_INSTANCE_ID --region us-west-2
+
